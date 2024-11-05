e5dc8f74cc0df9386afe5c108bf9a122790a0631
fantiniiii :) <126190358+fantinilind4@users.noreply.github.com>
2024-11-05 09:57:13 -0300

    Add files via upload

---
+++ continuação flashcard/perguntas.js
@@ -0,0 +1,15 @@
+criaCartao (
+    'Futebol',
+    'Qual time vai para a série B ano que vem?',
+    'O Corinthians'
+)
+criaCartao (
+    'Futebol',
+    'Qual é o melhor time do Brasil?',
+    'O São Paulo'
+)
+criaCartao (
+    'Futebol',
+    'Qual time não tem mundial?',
+    'O Palmeiras'
+)
