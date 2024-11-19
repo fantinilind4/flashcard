7a67505168003fad122637f852c1d2037c051792
fantiniiii :) <126190358+fantinilind4@users.noreply.github.com>
2024-11-19 10:04:26 -0300

    Add files via upload

--- continuação flashcard/perguntas.js
+++ continuação flashcard/perguntas.js
@@ -1,6 +1,6 @@
 criaCartao (
     'Futebol',
-    'Qual time vai para a série B ano que vem?',
+    'Qual é o pior time do mundo?',
     'O Corinthians'
 )
 criaCartao (
@@ -12,4 +12,19 @@ criaCartao (
     'Futebol',
     'Qual time não tem mundial?',
     'O Palmeiras'
+)
+criaCartao (
+    'Série',
+    'O jj morre?',
+    'infelizmente, (chorei, chorei)'
+)
+criaCartao (
+    'Saga',
+    'Velozes e Furiosos ou Harry Potter?',
+    'Tudo bem, eu posso amar os dois'
+)
+criaCartao (
+    'Sonho',
+    'Um sonho?',
+    'Ganhar um buquê'
 )
