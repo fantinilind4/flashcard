7097625bbc57bf2d4d4f00525aef812598ed892d
fantiniiii :) <126190358+fantinilind4@users.noreply.github.com>
2024-11-26 09:12:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao (
+    'Futebol',
+    'Qual é o pior time do mundo?',
+    'O Corinthians'
+)
+criaCartao (
+    'Futebol',
+    'Qual é o melhor time do Brasil?',
+    'O São Paulo'
+)
+criaCartao (
+    'Futebol',
+    'Qual time não tem mundial?',
+    'O Palmeiras'
+)
+criaCartao (
+    'Série',
+    'O jj morre?',
+    'infelizmente, (chorei, chorei)'
+)
+criaCartao (
+    'Saga',
+    'Velozes e Furiosos ou Harry Potter?',
+    'Tudo bem, eu posso amar os dois'
+)
+criaCartao (
+    'Sonho',
+    'Um sonho?',
+    'Ganhar um buquê'
+)
